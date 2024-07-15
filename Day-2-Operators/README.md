0d42723f9b41c51d677290da58f0d6f729574ff0
vannammanasa <vannammanasa2478@gmail.com>
2024-07-15 18:38:42 +0530

    Day-2-Operators

---
+++ Day-2-Operators/README.md
@@ -0,0 +1,12 @@
+<strong> By the end of these activities, cleared the concept of operators: </strong>
+<hr>
+
+- Understood the  usage of arithmetic operators to perform basic calculations.
+
+- Using assignment operators to modify variable values.
+
+- Comparing values using comparison operators.
+
+- Combining conditions using logical operators.
+
+- Using the ternary operator for concise conditional expressions.
---
+++ Day-2-Operators/app.js
@@ -0,0 +1,89 @@
+//Activity-1:-(Arithmetic Operators):-
+//1.Program to add 2 numbers & log the result to the console.
+let num1 = 7;
+let num2 = 15;
+console.log(num1 + num2);
+
+
+//2.Program to subtract 2 numbers & log the result to the console.
+let num1_sub = 17;
+let num2_sub = 15;
+console.log(num1_sub - num2_sub);
+//3.Program to multiply 2 numbers & log the result to the console.
+let num1_mul = 70;
+let num2_mul = 15;
+console.log(num1_mul * num2_mul);
+//4.Program to divide 2 numbers & log the result to the console.
+let num1_div = 7;
+let num2_div = 15;
+console.log(num1_div /  num2_div);
+
+
+//5.Program to find the remainder when one number is divided by another log the result to the console.
+
+let num1_rem = 7;
+let num2_rem = 15;
+console.log(num1_rem % num2_rem);
+
+
+
+//Activity-2 - (Assignment Operators):-
+//6.Use the += operator to add a number to a variable & log the result to the console
+
+let a = 7;
+a += 10
+console.log(a)
+
+//7.Use the -= operator to subtract a number to a variable & log the result to the console.
+let b = 37;
+b -= 10
+console.log(b)
+
+//Activity-3 - (Comparison Operators):-
+
+//8.Program to compare 2 numbers using > and < & log the result to the console.
+let d = 9;
+let e = 90;
+console.log(d>e)
+//9.Program to compare 2 numbers using >= and <= & log the result to the console.
+let f = 9;
+let g = 90;
+console.log(f >=g)
+
+//10.Program to compare 2 numbers using == and === & log the result to the console.
+let h = 190;
+let i = 500;
+console.log(h==i); //false
+
+let j = '40';
+let k = 40;
+console.log(j === k) //false
+
+
+//Activity-4 - (Logical Operators):-
+
+//11.Program that uses the && operator to combine two conditions & log the result to the console.
+let m = 80;
+let n = 80;
+console.log(m == n && m === n) //true
+
+//12.Program that uses the || operator to combine two conditions & log the result to the console..
+let o = 80;
+let p = 89;
+console.log(o <= p || o === p) //true
+
+//13.Program that uses the ! operator to negate a condition & log the result to the console.
+console.log(!true) //false
+
+
+//Activity-5-(Ternary Operator):-
+
+//4. Program that uses the ternary operator to check if a number is +ve or -ve
+console.log(-7 >= 0 ? 'postive':'negative') //negative
+
+
+//Feature Request:-
+//1.Arithmetic Operations Script - Create a Script that performs basic arithmetic operations(+,-,*,/)
+
+//2.Comparison & Logical Operators Script - Create a script that compares 2 numbers using diff.comparison operators  & combines conditions using logical operators,logging the result
+//3.Ternary Operator Script - Write a script that uses the ternary operator to determine if a number is +ve or -ve & logging the result.
